db575d1f12169d44bc98209101afac2a023698a6
Abanoub Ghadban <abanoub.gdb@gmail.com>
2019-10-22 01:42:35 +0200

    added des decryption

--- des.py
+++ des.py
@@ -16,3 +16,18 @@ def encrypt(msg:str, key:str):
     for i in range(0, len(final_cipher), 4):
         hex_cipher += bin_to_hex(joinNumbers(final_cipher[i:i+4]))
     return hex_cipher
+
+def decrypt(cipher:str, key:str) -> str:
+    keys = generateKeys(key)
+    keys.reverse()
+    text_bits = get_bits(cipher)
+
+    msg = []
+    for i in range(0, len(text_bits), 64):
+        msg.extend(encryptBlock(text_bits[i:i+64], keys))
+
+	# conversion of binary text into hex-decimal form
+    hex_msg = ''
+    for i in range(0, len(msg), 4):
+        hex_msg += bin_to_hex(joinNumbers(msg[i:i+4]))
+    return hex_msg.rstrip('\x00')
